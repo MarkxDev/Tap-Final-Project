9931139f3d131f6cba2f9102591494cf9fccb915
Marco Di Benedetto <35336860+MarkxDev@users.noreply.github.com>
2019-06-11 08:17:18 +0200

    Update prediction_to_csv.R

--- R/scripts/prediction_to_csv.R
+++ R/scripts/prediction_to_csv.R
@@ -1,14 +1,14 @@
 library(tm)
 prediction_to_csv <- function(id, text, sp){
 
+#create dataframe
 Tweet.id <- c(id)  #id
 Tweet.text <- c(text) #tweet
 Tweet.sentiment <- c(sp) #prediction
-output.df <- data.frame(df.id, df.text, df.sentiment)
+output.df <- data.frame(Tweet.id, Tweet.text, Tweet.sentiment)
 
+#Output file name
 excel.name <- "output.csv"
-
-# salva nuovo file
 
 # write to csv
 write.table(output.df,excel.name, append = T,
